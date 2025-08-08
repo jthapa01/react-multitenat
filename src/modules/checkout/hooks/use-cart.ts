634d9c4d544003f7d21449014d385fba16e5af20
jeevan thapa <jeevanthapa@microsoft.com>
2025-08-08 13:06:09 -0400

    21: Library implementation

--- src/modules/checkout/hooks/use-cart.ts
+++ src/modules/checkout/hooks/use-cart.ts
@@ -1,36 +1,45 @@
+import { useCallback } from "react";
+import { useShallow } from "zustand/shallow";
+
 import { useCartStore } from "../store/use-cart-store";
 
 export const useCart = (tenantSlug: string) => {
-  const {
-    getCartByTenant,
-    addProduct,
-    removeProduct,
-    clearCart,
-    clearAllCarts,
-  } = useCartStore();
+  // using zustand selectors to avoid unnecessary re-renders
+  const addProduct = useCartStore((state) => state.addProduct);
+  const removeProduct = useCartStore((state) => state.removeProduct);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const clearAllCarts = useCartStore((state) => state.clearAllCarts);
 
-  const productIds = getCartByTenant(tenantSlug);
+  const productIds = useCartStore(useShallow((state) => state.tenantCarts[tenantSlug]?.productIds || []));
 
-  const toggleProduct = (productId: string) => {
+  const toggleProduct = useCallback((productId: string) => {
     if (productIds.includes(productId)) {
       removeProduct(tenantSlug, productId);
     } else {
       addProduct(tenantSlug, productId);
     }
-  };
+  }, [productIds, addProduct, removeProduct, tenantSlug]);
 
-  const isProductInCart = (productId: string) => {
+  const isProductInCart = useCallback((productId: string) => {
     return productIds.includes(productId);
-  };
+  },[productIds]);
 
-  const clearTenantCart = () => {
+  const clearTenantCart = useCallback(() => {
     clearCart(tenantSlug);
-  };
+  }, [clearCart, tenantSlug]);
+
+  const handleAddProduct = useCallback((productId: string) => {
+    addProduct(tenantSlug, productId);
+  }, [addProduct, tenantSlug]);
+
+  const handleRemoveProduct = useCallback((productId: string) => {
+    removeProduct(tenantSlug, productId);
+  }, [removeProduct, tenantSlug]);
 
   return {
     productIds,
-    addProduct: (productId: string) => addProduct(tenantSlug, productId),
-    removeProduct: (productId: string) => removeProduct(tenantSlug, productId),
+    addProduct: handleAddProduct,
+    removeProduct: handleRemoveProduct,
     clearCart: clearTenantCart,
     clearAllCarts,
     toggleProduct,
--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -37,7 +37,7 @@ export const SearchInput = ({
             </Button>
             { session.data?.user && (
                 <Button asChild variant="elevated">
-                    <Link href="/library">
+                    <Link prefetch href="/library">
                         <BookmarkCheckIcon />
                         Library
                     </Link>
