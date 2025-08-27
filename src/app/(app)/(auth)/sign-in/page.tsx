e34d54014348bc56b60eed3bf8aa54a86660d63a
jeevan thapa <jeevanthapa@microsoft.com>
2025-08-27 09:54:06 -0400

    28: Deployment

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { caller } from "@/trpc/server";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
+export const dynamic = "force-dynamic";
 const Page = async () => {
   const session = await caller.auth.session();
   if (session.user) {
